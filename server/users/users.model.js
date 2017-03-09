447180a7529f24c4544ba5559581e6e6dabb31f5
artoocampus <fuscaldo.daniele93@gmail.com>
2017-03-09 12:01:14 +0100

    aggiunta

--- server/users/users.model.js
+++ server/users/users.model.js
@@ -2,12 +2,32 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 
 var userSchema = new schema({
-    nome: String,
-    cognome: String,
-    sesso: String,
-    età: Number,
+    nome: {
+        type: String,
+        required:[true,'devi inserire il nome']
+          },
+    cognome: {
+        type :String,
+        required:[true,'devi inserire il cognome'],
+        unique: [true,"cognome gia esistente"]
+},
+    sesso: {
+        type: String,
+        enum:['Male','Female']
+    },
+    età: {
+        type: Number,
+        required:true,
+        min:[18,"devi essere maggiorenne"],
+        max:100
+    },
 });
 
-var User = mongoose.model('User',userSchema);
+userSchema.pre('save', function(next){
+    this.nome = this.nome.charAt(0).toUpperCase() +
+        this.nome.substring(1).toLowerCase();
+        next();
+})
+ var User =mongoose.model('User',userSchema);
 
 module.exports = User;
